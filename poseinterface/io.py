0a737f5ff22d5dec2b5e131ad9cfd0a8da20274d
sfmig <33267254+sfmig@users.noreply.github.com>
2026-02-11 14:51:00 +0000

    Add function to update image IDs in COCO JSON and add DLC test fixtures (#19)
    * Update IDs for images, annotations and categories
    
    * Remove in-place dict modification
    
    * Add tests
    
    * Small edit to imports
    
    * Draft test annotations_to_coco
    
    * Update docstring
    
    * Remove comment
    
    * Remote annotations_to_coco test
    
    * Remove update of category and annotation ID
    
    * Remove comment
    
    * Add test data
    
    * Remove placeholder
    
    * Reorder tests. Add test for annotations_to_coco
    
    * Add frame_regexp as input
    
    * Add test fixtures
    
    * Rename tests
    
    * Add integration test
    
    * Add frame_regexp to tests
    
    * Implement review comments
    
    * Add pytest-lazy-fixture to development dependencies
    
    * Fix typo in pytest-lazy-fixture dependency name in pyproject.toml
    
    * Update tests/test_unit/test_io.py
    
    Co-authored-by: Chang Huan Lo <changhuan.lo@ucl.ac.uk>
    
    * Use session scope
    
    * Add single video check. Use poseinterface frameregexp in _update_image_ids
    
    * Fix tests
    
    * Improve error message for multiple video annotations in annotations_to_coco and add corresponding test
    
    * Use poseinterface regexp as default in _extract_frame_number
    
    ---------
    
    Co-authored-by: Chang Huan Lo <changhuan.lo@ucl.ac.uk>

--- poseinterface/io.py
+++ poseinterface/io.py
@@ -1,7 +1,11 @@
+import copy
+import json
+import re
 from pathlib import Path
 
 import sleap_io as sio
-from sleap_io.io import dlc
+from sleap_io.io import coco
+from sleap_io.io.dlc import is_dlc_file
 
 _EMPTY_LABELS_ERROR_MSG = {
     "default": (
@@ -17,6 +21,8 @@ _EMPTY_LABELS_ERROR_MSG = {
     ),
 }
 
+POSEINTERFACE_FRAME_REGEXP = r"frame-(\d+)"
+
 
 def annotations_to_coco(
     input_path: Path,
@@ -25,7 +31,7 @@ def annotations_to_coco(
     coco_image_filenames: str | list[str] | None = None,
     coco_visibility_encoding: str = "ternary",
 ) -> Path:
-    """Export annotations file to COCO format.
+    """Export annotations file from a single video to poseinterface format.
 
     Parameters
     ----------
@@ -55,6 +61,10 @@ def annotations_to_coco(
     on its extension. See :func:`sleap_io.io.main.load_file` for supported
     formats.
 
+    In the poseinterface COCO output file, the image IDs correspond to the
+    frame numbers and are derived from the image filenames. The annotation and
+    category ids are 0-based indices.
+
     Example
     -------
     >>> from pathlib import Path
@@ -65,16 +75,85 @@ def annotations_to_coco(
     ... )
     """
     labels = sio.load_file(input_path)
+
     # Check if labels object is empty
     if len(labels.labeled_frames) == 0:
         error_msg = _EMPTY_LABELS_ERROR_MSG["default"]
-        if dlc.is_dlc_file(input_path):
+        if is_dlc_file(input_path):
             error_msg += _EMPTY_LABELS_ERROR_MSG["dlc"]
         raise ValueError(error_msg)
-    sio.save_coco(
+
+    # Check single video
+    if len(labels.videos) > 1:
+        raise ValueError(
+            "The annotations refer to multiple videos "
+            f"(n={len(labels.videos)}). "
+            "Please check that the input file contains annotations "
+            "for a single video only."
+        )
+
+    # Generate COCO dict from sleap-io
+    # with PR19 image_filenames = poseinterface filenames
+    coco_data = coco.convert_labels(
         labels,
-        output_json_path,
         image_filenames=coco_image_filenames,
         visibility_encoding=coco_visibility_encoding,
     )
+
+    # Update image ids to match frame number
+    # uncomment after PR19
+    # coco_data = _update_image_ids(coco_data)
+
+    # Save JSON file
+    with open(output_json_path, "w") as f:
+        json.dump(coco_data, f)
+
     return output_json_path
+
+
+def _update_image_ids(input_data: dict) -> dict:
+    """Assigns new image IDs based on the frame number in the filename."""
+    # Create new dict
+    data = copy.deepcopy(input_data)
+
+    # Build map old-to-new image IDs and update image id in images list
+    old_to_new_id = {}
+    for img in data["images"]:
+        # map old image_id to new image_id
+        old_img_id = img["id"]
+        new_img_id = _extract_frame_number(img["file_name"])
+        old_to_new_id[old_img_id] = new_img_id
+
+        # update image_id in images list
+        img["id"] = new_img_id
+
+    # Check new image IDs are unique
+    if len(old_to_new_id) != len(set(old_to_new_id.values())):
+        raise ValueError(
+            "Extracted image IDs are not unique. Please check that the frame "
+            "numbers as specified in the filename are unique."
+        )
+
+    # Update image_id in annotations list
+    for annot in data["annotations"]:
+        annot["image_id"] = old_to_new_id[annot["image_id"]]
+
+    return data
+
+
+def _extract_frame_number(
+    filename: str, frame_regexp: str = POSEINTERFACE_FRAME_REGEXP
+) -> int | None:
+    """Extract the frame number in the input filename.
+
+    If no frame number is found, returns None.
+    """
+    match = re.search(frame_regexp, filename)
+    if match is None:
+        raise ValueError(
+            "No frame number could be extracted from filename "
+            f"{filename}. Please check that the filename contains a "
+            "frame number matching the provided regexp pattern "
+            rf"'{frame_regexp}'."
+        )
+    return int(match.group(1))
